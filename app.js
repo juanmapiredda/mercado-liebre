0005b6bd146d51964a98d5d6f972d788a847b515
juanmapiredda <juan_piredda@hotmail.com>
2021-09-27 20:57:41 -0300

    prueba heroku

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ app.get ('/' , (req,res) =>{
     res.sendFile(path.resolve(__dirname , './views/home.html'));
 })
 
-app.listen(3000 , ()=> console.log('Servidor corriendo en el puerto 3000'));
+app.listen(process.env.PORT || 3000,function () {
+    console.log(`Servidor corriendo en puerto 3000`)
+  });
 
 
